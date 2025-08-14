cce1dd4a222903b3655d9c308991667babe7402b
TheRagingAsura <undertakerryu@gmail.com>
2025-08-14 02:26:18 -0400

    feat: calc methods added, general outline of options listed and basic addition functionality created.

--- Calculator.java
+++ Calculator.java
@@ -1,2 +1,47 @@
+import java.util.Scanner;
+
 public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Select 1 for addition");
+        System.out.println("Select 2 for subtraction");
+        System.out.println("Select 3 for multiplication");
+        System.out.println("Select 4 for division");
+        System.out.print("User Selection: ");
+
+        int selection = scanner.nextInt();
+        while (selection != 5){
+            if (selection == 1) {
+                System.out.print("Enter first number: ");
+                int a = scanner.nextInt();
+                System.out.print("Enter second number: ");
+                int b = scanner.nextInt();
+                System.out.println("the total is " + (a + b));
+                //user input needed for next selection/exiting the calc
+            }
+        }
+
+
+
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int subtract(int a, int b){
+        return a - b;
+    }
+
+    public static int multiplication(int a, int b){
+        return a * b;
+    }
+
+    public static int divide(int a, int b){
+        return a / b;
+    }
+
+
 }
