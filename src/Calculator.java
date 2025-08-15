c58ca045c4a740de1d1081d7c221e5ed5a696225
TheRagingAsura <undertakerryu@gmail.com>
2025-08-15 01:47:35 -0400

    feat: Finalized menu options. Calculator complete for 2 value entries. Eventually re-vist to adjust to allow for longer strings to be entered.

--- src/Calculator.java
+++ src/Calculator.java
@@ -44,26 +44,5 @@ public class Calculator {
             selection = scanner.nextInt();
 
         }
-
-
-
     }
-
-    public static int add(int a, int b){
-        return a + b;
-    }
-
-    public static int subtract(int a, int b){
-        return a - b;
-    }
-
-    public static int multiplication(int a, int b){
-        return a * b;
-    }
-
-    public static int divide(int a, int b){
-        return a / b;
-    }
-
-
 }
